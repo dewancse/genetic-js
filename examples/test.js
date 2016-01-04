f49b754971db159a01ab8f9d33edd87775511bdd
Dewan Mahabub Sarwar <sarwarcse@gmail.com>
2016-01-04 13:23:17 +1300

    added seed function

--- examples/test.js
+++ examples/test.js
@@ -11,7 +11,30 @@ var x = function () {
     genetic.select1 = Genetic.Select1.Tournament2;
     genetic.select2 = Genetic.Select2.Tournament2;
 
-    
+    genetic.seed = function () {
+
+        /*
+         * create random seed that are equal in length to solution
+         * If random number is divisible by 2 Then put 1
+         * Else put 0
+         */
+
+        console.log("SEED");
+        var len = this.userData["edges"].length;
+        var i = Math.floor(Math.random() * len);
+
+        var a = [];
+        for (var j = 0; j < len; j++) {
+            var i = Math.floor(Math.random() * len);
+            if (i % 2 == 0)
+                a[j] = 1;
+            else
+                a[j] = 0;
+        }
+
+        return a;
+    };
+
     console.log("Hello");
     var g = new Graph();
     console.log(g);
