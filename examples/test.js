82506a013505fa8c3ec7de93cee77a183dc65042
Dewan Mahabub Sarwar <sarwarcse@gmail.com>
2016-01-04 13:32:13 +1300

    added draw function

--- examples/test.js
+++ examples/test.js
@@ -227,6 +227,150 @@ var x = function () {
         }
     };
 
+    function draw(result) {
+        var links = [];
+        for (var i = 0; i < result.length; i++) {
+            links.push({
+                source: result[i][0],
+                target: result[i][1],
+                weight: result[i][2],
+                species: result[i][3]
+            });
+        }
+
+        var nodes = {};
+
+        // Compute the distinct nodes from the links.
+        links.forEach(function (link) {
+            link.source = nodes[link.source] ||
+                (nodes[link.source] = {name: link.source});
+            //console.log(link.source);
+
+            link.target = nodes[link.target] ||
+                (nodes[link.target] = {name: link.target});
+            //console.log(link.target);
+        });
+
+        var g = document.getElementById("#svgVisualize"),
+            width = window.innerWidth,
+            height = window.innerHeight;
+
+        var svg = d3.select("#svgVisualize").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+
+        function updateWindow() {
+            width = window.innerWidth;
+            height = window.innerHeight;
+            svg.attr("width", width).attr("height", height);
+        }
+
+        window.onresize = updateWindow;
+
+        var color = d3.scale.category20();
+
+        var force = d3.layout.force()
+            .nodes(d3.values(nodes))
+            .links(links)
+            .size([width, height])
+            .linkDistance(300)
+            .charge(-1000)
+            .on("tick", tick)
+            .start();
+
+        // build the arrow.
+        svg.append("svg:defs").selectAll("marker")
+            .data(["end"])      // Different link/path types can be defined here
+            .enter().append("svg:marker")    // This section adds in the arrows
+            .attr("id", String)
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 15)
+            .attr("refY", -1.5)
+            .attr("markerWidth", 6)
+            .attr("markerHeight", 6)
+            .attr("orient", "auto")
+            .append("svg:path")
+            .attr("d", "M0,-5L10,0L0,5");
+
+        //filter unique species from the result
+        var species = [];
+        var py = 20;
+
+        for (var i = 0; i < result.length; i++) {
+            species[i] = result[i][3];
+        }
+
+        species = species.filter(function (item, pos) {
+            return species.indexOf(item) == pos;
+        })
+
+        // add the links and the arrows
+        var path = svg.append("svg:g").selectAll("path")
+            .data(force.links())
+            .enter().append("svg:path")
+            .attr("class", "link")
+            .style("stroke", function (d) {
+                for (var i = 0; i < species.length; i++) {
+                    if (d.species == species[i]) {
+                        svg.append("text")
+                            .style("font", "14px sans-serif")
+                            .attr("stroke", color(d.species))
+                            .attr("x", 10)
+                            .attr("y", py)
+                            .text(d.species)
+
+                        //forward one step to get distinct color
+                        color(d.species + 1);
+                        py = py + 20;
+                        species[i] = "";
+                        break;
+                    }
+                }
+
+                return color(d.species);
+            })
+            .attr("marker-end", "url(#end)");
+
+        // define the nodes
+        var node = svg.selectAll(".node")
+            .data(force.nodes())
+            .enter().append("g")
+            .attr("class", "node")
+            .call(force.drag);
+
+        // add the nodes
+        node.append("circle")
+            .attr("r", 5);
+
+        // add the text
+        node.append("text")
+            .attr("x", 12)
+            .attr("dy", ".35em")
+            .text(function (d) {
+                return d.name;
+            });
+
+        // add the curvy lines
+        function tick() {
+            path.attr("d", function (d) {
+                var dx = d.target.x - d.source.x,
+                    dy = d.target.y - d.source.y,
+                    dr = Math.sqrt(dx * dx + dy * dy);
+                return "M" +
+                    d.source.x + "," +
+                    d.source.y + "A" +
+                    dr + "," + dr + " 0 0,1 " +
+                    d.target.x + "," +
+                    d.target.y;
+            });
+
+            node.attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+        }
+    }
+
     console.log("Hello");
     var g = new Graph();
     console.log(g);
